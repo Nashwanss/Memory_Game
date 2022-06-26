c80a474582c27a012ad1c1fd093a165842cc3846
Nashwan Kurdali <nashwankurdali@gmail.com>
2022-06-26 14:41:04 +0200

    Readme file has been updeated

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# Memory_Game
-Funny memory game using the Front-end technology logos.
-Enjoy & have a fun :)
+# _Memory Game_
+#### Table of contents
+* [General info](#general-information)
+* [Technologies](#Technologies)
+* [Setup & Launch](#Setup-&-Launch)
+* [Preview](#Preview)
+#### Preview
+![GIF](./memo_game.gif)
+
+#### General information
+Funny game using the Front-end technology logos as flash cards.
+It's fun and suitable for all ages category. (I did another one utilising disney catoons charachters to attract the younger enftant category with different sound effects).
+#### Technologies
+Actually , it's very simple project using only:
+- HTML.
+- CSS.
+- JavaScript.
+
+#### Setup & Launch
+This app can run very smooth on the local environmment, so no need to any special setup.
+```
+You can run the app directly via the HTML file
+Or
+By using the VS-Code extention (Go-Live)
+```
+Also you can find a production versioin below:
+| Platform | Link |
+| ------ | ------ |
+| Heroku | www.heroku.blablabla.com
+**Enjoy & Have Fun !**
+
+![GIF](https://c.tenor.com/YxJXXGG3WsUAAAAC/cat-have-a-good-day.gif)
--- index.js
+++ index.js
@@ -1,91 +1,115 @@
-document.querySelector('.control-buttons span').onclick=function() {
-    let yourName = prompt("Enter your name please ?")
+document.querySelector(".control-buttons span").onclick = function () {
+  let yourName = prompt("Enter your name please ?");
 
-    if (yourName === null || yourName === '' ) {
-        document.querySelector('.name span').innerHTML = 'Unknown user';
-        document.querySelector('.name span').style.color = 'red';
-    }else{
-        document.querySelector('.name span').innerHTML = `Hello ${yourName} 🤠 `;
-    }
-    document.querySelector('.control-buttons').remove();
+  if (yourName === null || yourName === "") {
+    document.querySelector(".name span").innerHTML = "Unknown user";
+    document.querySelector(".name span").style.color = "red";
+  } else {
+    document.querySelector(".name span").innerHTML = `Hello ${yourName} 🤠 `;
+  }
+  document.querySelector(".control-buttons").remove();
 };
 let duration = 500;
-let blocksContainer = document.querySelector('.memory-game-blocks');
+let blocksContainer = document.querySelector(".memory-game-blocks");
 let blocks = Array.from(blocksContainer.children);
 let orderRange = Array.from(Array(blocks.length).keys());
 shuffle(orderRange);
 
 blocks.forEach((block, index) => {
-    block.style.order = orderRange[index];
+  block.style.order = orderRange[index];
 
-block.addEventListener('click' , function ()  {
-    flipBlock(block)
-})
-})
+  block.addEventListener("click", function () {
+    flipBlock(block);
+  });
+});
 
 function flipBlock(selectedCard) {
-    selectedCard.classList.add('is-flipped');
-    // Collect all the flipped cards
-    let allFlippedBlocks = blocks.filter(flippedBlock => flippedBlock.classList.contains('is-flipped'));
-    // If two blocks has been selected:
-    if(allFlippedBlocks.length === 2) {
-        stopClick()
-        // Check if the two cards are matched
-        checkMatchedCards( allFlippedBlocks[0],allFlippedBlocks[1])
-    }
+  selectedCard.classList.add("is-flipped");
+  // Collect all the flipped cards
+  let allFlippedBlocks = blocks.filter((flippedBlock) =>
+    flippedBlock.classList.contains("is-flipped")
+  );
+  // If two blocks has been selected:
+  if (allFlippedBlocks.length === 2) {
+    stopClick();
+    // Check if the two cards are matched
+    checkMatchedCards(allFlippedBlocks[0], allFlippedBlocks[1]);
+  }
 
-    function stopClick() {
-        blocksContainer.classList.add('no-clicking');
-    setTimeout(()=> {
-        blocksContainer.classList.remove('no-clicking');
-    } , duration);
-    }
+  function stopClick() {
+    blocksContainer.classList.add("no-clicking");
+    setTimeout(() => {
+      blocksContainer.classList.remove("no-clicking");
+    }, duration);
+  }
 }
 
-function checkMatchedCards(firstCard , secondCard ) {
-    let triesElement = document.querySelector('.tries span');
-    if(firstCard.dataset.technology === secondCard.dataset.technology) {
-        firstCard.classList.remove('is-flipped');
-        secondCard.classList.remove('is-flipped');
+const triesElement = document.querySelector(".tries span");
 
-        firstCard.classList.add('has-match');
-        secondCard.classList.add('has-match');
+function checkMatchedCards(firstCard, secondCard) {
+  if (firstCard.dataset.technology === secondCard.dataset.technology) {
+    firstCard.classList.remove("is-flipped");
+    secondCard.classList.remove("is-flipped");
 
-        document.getElementById('success').play();
-    
-    }else
-        triesElement.innerHTML= parseInt(triesElement.innerHTML )+1;
-        setTimeout(function(){
-            firstCard.classList.remove('is-flipped');
-            secondCard.classList.remove('is-flipped');
-        },duration)
-        document.getElementById('fail').play();
-    
+    firstCard.classList.add("has-match");
+    secondCard.classList.add("has-match");
+
+    document.getElementById("success").play();
+  } else {
+    triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+    setTimeout(function () {
+      firstCard.classList.remove("is-flipped");
+      secondCard.classList.remove("is-flipped");
+    }, duration);
+
+    document.getElementById("fail").play();
+
+    // triesCounter();
     victory();
-    }
+  }
+}
 
-    function victory () {
-        const cards = document.querySelectorAll('.game-block');
-        let matchedCards = document.querySelectorAll('.has-match');
-        console.log(cards.length);
-        console.log(matchedCards.length);
-        if (cards.length === matchedCards.length){
-            console.log('Bravooo')
-            document.getElementById('victory').play();
-        }
-    }
-    
-    function shuffle(arr) {
-    let current = arr.length , temp , random;
-    while (current > 0) {
-        random = (Math.floor(Math.random() * current));
+function victory() {
+  const cards = document.querySelectorAll(".game-block");
+  let matchedCards = document.querySelectorAll(".has-match");
+  console.log(cards.length);
+  console.log(matchedCards.length);
+  if (cards.length === matchedCards.length) {
+    console.log("Bravooo");
+    document.getElementById("victory").play();
+  }
+}
+
+function triesCounter() {
+  if (triesElement.innerHTML > 2) {
+    console.log("Game Over");
+    gameOver();
+  }
+}
+
+
+function gameOver() {
+  // var txt;
+  if (confirm("Press a button!")) {
+    console.log("Press a button")
+    // txt = "You pressed OK!";
+  } else {
+    console.log('You pressed Cancel!')
+    // txt = "You pressed Cancel!";
+  }
+}
+
+
+function shuffle(arr) {
+  let current = arr.length,
+    temp,
+    random;
+  while (current > 0) {
+    random = Math.floor(Math.random() * current);
     current--;
     temp = arr[current];
     arr[current] = arr[random];
     arr[random] = temp;
+  }
+  return arr;
 }
-return arr;
-}
-
-
-
